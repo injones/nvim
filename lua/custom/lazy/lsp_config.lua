333cd0d968a74af7808f7c8e8d696c51204d4c93
injones <ieuanj28@googlemail.com>
2024-10-13 21:34:41 +0100

    add gitsigns, plus lsp updates

--- lua/custom/lazy/lsp_config.lua
+++ lua/custom/lazy/lsp_config.lua
@@ -125,6 +125,9 @@ return {
 									semicolon = "Disable",
 									arrayIndex = "Disable",
 								},
+								diagnostics = {
+									globals = { "vim" },
+								},
 							},
 						},
 					},
