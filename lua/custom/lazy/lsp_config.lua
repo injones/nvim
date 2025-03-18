cea67373e61e1f234e7d474b5602213abccc8fa9
injones <ieuanj28@googlemail.com>
2025-03-18 23:07:19 +0000

    update lsp config (trying to clean it up)

--- lua/custom/lazy/lsp_config.lua
+++ lua/custom/lazy/lsp_config.lua
@@ -1,4 +1,353 @@
----@class LazyKeysBase
+return {
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"stevearc/conform.nvim",
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"j-hui/fidget.nvim",
+        'WhoIsSethDaniel/mason-tool-installer.nvim',
+	    { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
+	},
+
+	config = function()
+		vim.api.nvim_create_autocmd("LspAttach", {
+			-- from nvim kickstart
+			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+			callback = function(event)
+				-- NOTE: Remember that Lua is a real programming language, and as such it is possible
+				-- to define small helper and utility functions so you don't have to repeat yourself.
+				--
+				-- In this case, we create a function that lets us more easily define mappings specific
+				-- for LSP related items. It sets the mode, buffer and description for us each time.
+				local map = function(keys, func, desc, mode)
+					mode = mode or "n"
+					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+				end
+
+				-- Jump to the definition of the word under your cursor.
+				--  This is where a variable was first declared, or where a function is defined, etc.
+				--  To jump back, press <C-t>.
+				map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+
+				-- Find references for the word under your cursor.
+				map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+
+				-- Jump to the implementation of the word under your cursor.
+				--  Useful when your language has ways of declaring types without an actual implementation.
+				map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+
+				-- Jump to the type of the word under your cursor.
+				--  Useful when you're not sure what type a variable is and you want to see
+				--  the definition of its *type*, not where it was *defined*.
+				map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+
+				-- Fuzzy find all the symbols in your current document.
+				--  Symbols are things like variables, functions, types, etc.
+				map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+
+				-- Fuzzy find all the symbols in your current workspace.
+				--  Similar to document symbols, except searches over your entire project.
+				map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+				-- Rename the variable under your cursor.
+				--  Most Language Servers support renaming across files, etc.
+				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+
+				-- Execute a code action, usually your cursor needs to be on top of an error
+				-- or a suggestion from your LSP for this to activate.
+				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
+
+				-- WARN: This is not Goto Definition, this is Goto Declaration.
+				--  For example, in C this would take you to the header.
+				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+				-- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+				---@param client vim.lsp.Client
+				---@param method vim.lsp.protocol.Method
+				---@param bufnr? integer some lsp support methods only in specific files
+				---@return boolean
+				local function client_supports_method(client, method, bufnr)
+					if vim.fn.has("nvim-0.10") == 1 then
+						return client:supports_method(method, bufnr)
+					else
+						return client.supports_method(method, { bufnr = bufnr })
+					end
+				end
+
+				-- The following two autocommands are used to highlight references of the
+				-- word under your cursor when your cursor rests there for a little while.
+				--    See `:help CursorHold` for information about when this is executed
+				--
+				-- When you move your cursor, the highlights will be cleared (the second autocommand).
+				local client = vim.lsp.get_client_by_id(event.data.client_id)
+				if
+					client
+					and client_supports_method(
+						client,
+						vim.lsp.protocol.Methods.textDocument_documentHighlight,
+						event.buf
+					)
+				then
+					local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.document_highlight,
+					})
+
+					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.clear_references,
+					})
+
+					vim.api.nvim_create_autocmd("LspDetach", {
+						group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+						callback = function(event2)
+							vim.lsp.buf.clear_references()
+							vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+						end,
+					})
+				end
+
+				-- The following code creates a keymap to toggle inlay hints in your
+				-- code, if the language server you are using supports them
+				--
+				-- This may be unwanted, since they displace some of your code
+				if
+					client
+					and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
+				then
+					map("<leader>th", function()
+						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+					end, "[T]oggle Inlay [H]ints")
+				end
+
+                -- setup compiler config for omnisharp
+                -- https://github.com/Hoffs/omnisharp-extended-lsp.nvim/issues/42#issuecomment-2480414792
+                if client and client.name == "omnisharp" then
+                    map('gd', require('omnisharp_extended').lsp_definition, '[G]oto [D]efinition')
+                    map('gr', require('omnisharp_extended').lsp_references, '[G]oto [R]eferences')
+                    map('gI', require('omnisharp_extended').lsp_implementation, '[G]oto [I]mplementation')
+                    map('<leader>D', require('omnisharp_extended').lsp_type_definition, 'Type [D]efinition')
+                end
+			end,
+		})
+
+		local cmp = require("cmp")
+		local cmp_lsp = require("cmp_nvim_lsp")
+		local luasnip = require("luasnip")
+		luasnip.config.setup({})
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities()
+		)
+
+        local servers = {
+            -- clangd = {},
+            -- gopls = {},
+            -- pyright = {},
+            -- rust_analyzer = {},
+            -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+            --
+            -- Some languages (like typescript) have entire language plugins that can be useful:
+            --    https://github.com/pmizio/typescript-tools.nvim
+            --
+            -- But for many setups, the LSP (`ts_ls`) will work just fine
+            -- ts_ls = {},
+            --
+
+            lua_ls = {
+              -- cmd = { ... },
+              -- filetypes = { ... },
+              -- capabilities = {},
+              settings = {
+                Lua = {
+                  completion = {
+                    callSnippet = 'Replace',
+                  },
+                  -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+                  -- diagnostics = { disable = { 'missing-fields' } },
+                },
+              },
+            },
+
+            omnisharp = {
+			    --cmd = { "dotnet", "/path/to/omni/OmniSharp.dll" },
+                -- The following doesn't work for some reason
+                handlers = {
+                    ["textDocument/definition"] = function(...)
+                        return require("omnisharp_extended").handler(...)
+                    end,
+                },
+                keys = {
+                    {
+                        "gd",
+                        function()
+                            require("omnisharp_extended").telescope_lsp_definitions()
+                        end,
+                        noremap = true,
+                        desc = "Goto Definition",
+                    },
+                },
+                --
+                enable_roslyn_analyzers = true,
+                organize_imports_on_format = true,
+                enable_import_completion = true,
+                settings = {
+                    FormattingOptions = {
+                        -- Enables support for reading code style, naming convention and analyzer
+                        -- settings from .editorconfig.
+                        EnableEditorConfigSupport = true,
+                        -- Specifies whether 'using' directives should be grouped and sorted during
+                        -- document formatting.
+                        OrganizeImports = true,
+                    },
+                    MsBuild = {
+                        -- If true, MSBuild project system will only load projects for files that
+                        -- were opened in the editor. This setting is useful for big C# codebases
+                        -- and allows for faster initialization of code navigation features only
+                        -- for projects that are relevant to code that is being edited. With this
+                        -- setting enabled OmniSharp may load fewer projects and may thus display
+                        -- incomplete reference lists for symbols.
+                        LoadProjectsOnDemand = nil,
+                    },
+                    RoslynExtensionsOptions = {
+                        -- Enables support for roslyn analyzers, code fixes and rulesets.
+                        EnableAnalyzersSupport = true,
+                        -- Enables support for showing unimported types and unimported extension
+                        -- methods in completion lists. When committed, the appropriate using
+                        -- directive will be added at the top of the current file. This option can
+                        -- have a negative impact on initial completion responsiveness,
+                        -- particularly for the first few completion sessions after opening a
+                        -- solution.
+                        EnableImportCompletion = nil,
+                        -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+                        -- true
+                        AnalyzeOpenDocumentsOnly = nil,
+                    },
+                    Sdk = {
+                        -- Specifies whether to include preview versions of the .NET SDK when
+                        -- determining which version to use for project loading.
+                        IncludePrereleases = true,
+                    },
+                }
+            }
+          }
+
+        local ensure_installed = vim.tbl_keys(servers or {})
+          vim.list_extend(ensure_installed, {
+            'stylua', -- Used to format Lua code
+          })
+          require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+		require("fidget").setup({})
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {},
+			handlers = {
+              function(server_name)
+                local server = servers[server_name] or {}
+                -- This handles overriding only values explicitly passed
+                -- by the server configuration above. Useful when disabling
+                -- certain features of an LSP (for example, turning off formatting for ts_ls)
+                server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+                require('lspconfig')[server_name].setup(server)
+              end,
+            },
+		})
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+				end,
+			},
+			completion = { completeopt = "menu,menuone,noinsert" },
+			mapping = cmp.mapping.preset.insert({
+				-- Select the [n]ext item
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				-- Select the [p]revious item
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+
+				-- Scroll the documentation window [b]ack / [f]orward
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+				-- Accept ([y]es) the completion.
+				--  This will auto-import if your LSP supports it.
+				--  This will expand snippets if the LSP sent a snippet.
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+
+				-- If you prefer more traditional completion keymaps,
+				-- you can uncomment the following lines
+				--['<CR>'] = cmp.mapping.confirm { select = true },
+				--['<Tab>'] = cmp.mapping.select_next_item(),
+				--['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+				-- Manually trigger a completion from nvim-cmp.
+				--  Generally you don't need this, because nvim-cmp will display
+				--  completions whenever it has completion options available.
+				["<C-Space>"] = cmp.mapping.complete({}),
+
+				-- Think of <c-l> as moving to the right of your snippet expansion.
+				--  So if you have a snippet that's like:
+				--  function $name($args)
+				--    $body
+				--  end
+				--
+				-- <c-l> will move you to the right of each of the expansion locations.
+				-- <c-h> is similar, except moving you backwards.
+				["<C-l>"] = cmp.mapping(function()
+					if luasnip.expand_or_locally_jumpable() then
+						luasnip.expand_or_jump()
+					end
+				end, { "i", "s" }),
+				["<C-h>"] = cmp.mapping(function()
+					if luasnip.locally_jumpable(-1) then
+						luasnip.jump(-1)
+					end
+				end, { "i", "s" }),
+
+				-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+			}),
+			sources = {
+				{
+					name = "lazydev",
+					-- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+					group_index = 0,
+				},
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "path" },
+				{ name = "nvim_lsp_signature_help" },
+			},
+		})
+
+		vim.diagnostic.config({
+			-- update_in_insert = true,
+			float = {
+				focusable = false,
+				style = "minimal",
+				border = "rounded",
+				source = "always",
+				header = "",
+				prefix = "",
+			},
+		})
+	end,
+}
+
+--[[ ---@class LazyKeysBase
 ---@field desc? string
 ---@field noremap? boolean
 ---@field remap? boolean
@@ -149,6 +498,16 @@ return {
 						enable_roslyn_analyzers = true,
 						organize_imports_on_format = true,
 						enable_import_completion = true,
+                        settings = {
+                            FormattingOptions = {
+                                -- Enables support for reading code style, naming convention and analyzer
+                                -- settings from .editorconfig.
+                                EnableEditorConfigSupport = true,
+                                -- Specifies whether 'using' directives should be grouped and sorted during
+                                -- document formatting.
+                                OrganizeImports = true,
+                            },
+                        }
 					},
 				},
 				-- you can do any additional lsp server setup here
@@ -487,4 +846,4 @@ return {
 			end)
 		end,
 	},
-}
+} ]]
