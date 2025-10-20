8bc5302ce084c575ac300840565c46ea9727ca5c
Ieuan Jones <ieuanj28@googlemail.com>
2025-10-20 20:35:14 +0100

    add rhubarb, clangd

--- lua/custom/lazy/lsp_config.lua
+++ lua/custom/lazy/lsp_config.lua
@@ -242,6 +242,23 @@ return {
 					},
 				},
 			},
+
+			clangd = {
+				cmd = {
+					"clangd",
+					"--background-index",
+					"--clang-tidy",
+					"--header-insertion=iwyu",
+					"--completion-style=detailed",
+					"--function-arg-placeholders",
+					"--fallback-style=llvm",
+				},
+				init_options = {
+					usePlaceholders = true,
+					completeUnimported = true,
+					clangdFileStatus = true,
+				},
+			},
 		}
 
 		local ensure_installed = vim.tbl_keys(servers or {})
